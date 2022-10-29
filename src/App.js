3d816941b3021af608bf9f4f2fd50fa607814a51
hassan khan <hf610061@gmail.com>
2022-10-29 15:46:26 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,61 +1,44 @@
+import './App.css';
+import Header from './components/navbar'
+// import { Formik} from 'formik';
+// import { useFormik } from 'formik';
+// import * as yup from 'yup';
+// import * as yup from 'yup';
 
-import "./App.css"
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-
-const App = () => {  
-  const validationSchema = yup.object({
-  email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
-  password: yup
-    .string('Enter your password')
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
-});
-
-  const formik = useFormik({
-    initialValues: {
-      email: 'foobar@example.com',
-      password: 'foobar',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
-  return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <p className='text-[red]'>this is para</p>
-        <input
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+function App() {
+     return (
+   <Header/>
   );
 }
+
 export default App;
+
+
+    /* <Formik
+      initialValues={{
+        fName: '',
+        lName: '',
+        email: '',
+      }}
+      onSubmit={(values) => {
+        console.log("value:", values)
+      }}
+      >
+      <Form>
+      <div>
+      <label htmlFor="firstName">First Name</label>
+      <Field id="firstName" name="fName" placeholder="Jane" />
+        </div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field style={{color: "red"}}id="lastName" name="lName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+        id="email"
+        name="email"
+        placeholder="jane@acme.com"
+        type="email"
+        />
+        <button type="submit">Submit</button>
+        </Form>
+      </Formik> */
